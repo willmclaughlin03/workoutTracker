daf0fa7835bd82d13d3212fd4770b9d01ea5b1a3
Will <willmclaughlin4th@gmail.com>
2026-01-09 13:28:21 -0500

    supabase login logic

---
+++ client/src/auth/authService.js
@@ -0,0 +1,22 @@
+import { supabase } from "../lib/supabaseClient";
+
+
+export const signUp = async (email, password) => {
+    const { data, error } = await supabase.auth.signUp({
+        email, password
+    });
+    if (error) throw error;
+    return data;
+}
+
+export const signIn = async (email, password) => {
+    const { data, error} = await supabase.auth.signInWithPassword({
+        email, password
+    });
+    if (error) throw error;
+    return data.session;
+}
+
+export const signOut = async () =>{
+    await supabase.auth.signOut();
+}
