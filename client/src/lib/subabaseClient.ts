5298890f1296146282089fd511c7a1ed16854760
Will <willmclaughlin4th@gmail.com>
2026-01-09 13:19:12 -0500

    frontend connection

---
+++ client/src/lib/subabaseClient.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js"
+import "dotenv/config";
+
+const supabase_url = process.env.SUPABASE_URL;
+const supabase_front_key = process.env.SUPABASE_ANON_KEY;
+
+if (!supabase_front_key || !supabase_url){
+    throw new Error("Improper Supabase Key or URL found");
+}
+
+export const supabase = createClient(supabase_url,supabase_front_key)
